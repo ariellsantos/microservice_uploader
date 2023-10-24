c19ce6c6599dfbbb1ae5e9a81b5cc8936128c4c6
Ariel Santos <ariel.easc@gmail.com>
2023-10-24 17:58:00 -0600

    localstorage service added

---
+++ src/Uploader/infrastructure/storage/CantClearStorageError.ts
@@ -0,0 +1,1 @@
+export class CantClearStorageError extends Error {}
---
+++ src/Uploader/infrastructure/storage/LocalDiskStorage/LocalStorage.ts
@@ -0,0 +1,62 @@
+import { FileData, Storage } from '../../../application/Storage';
+import { mkdir, readFile, rm, writeFile } from 'fs/promises';
+import { existsSync } from 'fs';
+import * as path from 'path';
+import { UploadFileError } from '../UploadFileError';
+import { LocalStorageConfigurations } from '../../../../Application/configs/configFactory';
+import { FileNotFoundError } from '../FileNotFoundError';
+import * as url from 'url';
+
+export class LocalStorage implements Storage {
+  constructor(private readonly localStorageConfig: LocalStorageConfigurations) {}
+
+  async createURL(fileName: string): Promise<string> {
+    if (!(await this.fileExists(fileName))) {
+      throw new FileNotFoundError(`This files doesn't exists`);
+    }
+    return url.format({
+      protocol: 'http',
+      hostname: this.localStorageConfig.host,
+      pathname: url.pathToFileURL(path.join('/', this.localStorageConfig.rootPath, fileName)).pathname
+    });
+  }
+
+  async findFile(fileName: string): Promise<FileData> {
+    try {
+      const filePath = path.join(this.localStorageConfig.rootDir, fileName);
+
+      return await readFile(filePath, { flag: 'r' });
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+      throw new FileNotFoundError('File not found');
+    }
+  }
+
+  async uploadFileFromBuffer(fileName: string, fileData: FileData): Promise<void> {
+    try {
+      const filePath = path.join(this.localStorageConfig.rootDir, fileName);
+      await this.ensureDirExist(filePath);
+      await writeFile(filePath, fileData);
+    } catch (error) {
+      if (error instanceof Error) console.log(error.message);
+      throw new UploadFileError(`Can't upload file`);
+    }
+  }
+
+  async fileExists(fileName: string): Promise<boolean> {
+    const filePath = path.join(this.localStorageConfig.rootDir, fileName);
+    return existsSync(filePath);
+  }
+
+  private async ensureDirExist(filePath: string): Promise<void> {
+    const dirname = path.dirname(filePath);
+    if (existsSync(dirname)) {
+      return;
+    }
+    await this.ensureDirExist(dirname);
+    await mkdir(dirname);
+  }
+  async clearStorage() {
+    await rm(this.localStorageConfig.rootDir, { recursive: true });
+  }
+}
